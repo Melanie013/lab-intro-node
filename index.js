1dc026c1b4e6fa3c7bee72f37964cbee26f46c9e
Melanie Schmotz <melanie.schmotz@web.de>
2022-04-12 14:24:09 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
 
-  add(item) {}
+    this.items = [];
+    this.length = this.items.length
 
-  get(pos) {}
+  }
 
-  max() {}
+  add(item) {
 
-  min() {}
+    this.items.push(item)
+    this.items.sort(function(a, b){
+      return a-b
 
-  sum() {}
+    })
+    this.length++
+  }
 
-  avg() {}
+  get(pos) {
+
+    if (pos < this.length) return this.items[pos];
+    else throw new Error('OutOfBounds');
+
+  }
+
+  max(items) {
+
+    if (this.items.length === 0) throw new Error('EmptySortedList')
+    else 
+    return Math.max(...this.items)
+  }
+
+  min() {
+
+    if (this.items.length === 0) throw new Error('EmptySortedList')
+    else 
+    return Math.min(...this.items)
+  }
+
+  sum() {
+
+    const totalSum = this.items.reduce(function(acc, val){
+      return acc += val
+
+    },0)
+      return totalSum;
+  }
+
+  avg() {
+
+
+    if (this.items.length === 0) throw new Error('EmptySortedList')
+
+    const totalSum = this.items.reduce(function(acc, val){
+      return acc += val
+
+    },0)
+      return totalSum / this.items.length;
+      
+  }
 }
 
 module.exports = SortedList;
